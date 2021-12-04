c8fb476282e09844af7d12911018773cc5bc9945
thirtsoft <thirtsoft@gmail.com>
2021-12-04 15:05:29 +0000

    fix url to heroku serveur

--- src/main/java/com/website/security/WebSecurityConfig.java
+++ src/main/java/com/website/security/WebSecurityConfig.java
@@ -62,8 +62,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 //        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-                registry.addMapping("/**").allowedOrigins("http://localhost:5000");
-                //    registry.addMapping("/**").allowedOrigins("https://medic-website.herokuapp.com");
+                // registry.addMapping("/**").allowedOrigins("http://localhost:5000");
+                    registry.addMapping("/**").allowedOrigins("https://medic-website.herokuapp.com");
 
             }
         };
